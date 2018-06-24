d0a3f8c8da851ce44e86a8c907cf863f57f779f6
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-24 16:03:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,13 +8,15 @@ class Song
   def self.new_by_filename(file)
     file = file.split(" - ")
     song = self.new(file[1])
-    artist_name=(file[0])
+    artist_name=(file[0], self)
+    self.artist.
     song
   end
 
   def artist_name=(name, some_song)
     Artist.find_or_create_by_name(name)
     Artist.add_song(some_song)
+    Artist
   end
 
 end
