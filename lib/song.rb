1e6ee98a1ac22e1754bf7ac864c27c212c429824
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-24 16:04:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   def self.new_by_filename(file)
     file = file.split(" - ")
     song = self.new(file[1])
-    artist_name=(file[0], self)
-    #self.artist = artist_name
+    artist_name=(file[0])
+    self.artist = artist_name
     song
   end
 
